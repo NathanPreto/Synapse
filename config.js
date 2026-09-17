fd21ac9c23b40efa163880641cdbeb8c8c162e0e
Nathan Preto Malicheski <nathanpretom@gmail.com>
2026-09-17 10:32:22 -0300

    Fix supabaseUrl in config.js

--- config.js
+++ config.js
@@ -4,6 +4,6 @@
  * Nunca coloque aqui uma secret key ou service_role.
  */
 window.SYNAPSE_CONFIG = {
-  supabaseUrl: 'https://bvdsfgzvubynxkjxyxc.supabase.co',
+  supabaseUrl: 'https://bvdsfgzvubynxjkjxyxc.supabase.co',
   supabaseKey: 'sb_publishable_-JQZeqUtRmVHgXh5LsnjWA_efRlb8YU'
 };
